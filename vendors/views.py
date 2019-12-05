c769c99c17d89011b781282c603daecb119024fd
bhadley <pebkak@gmail.com>
2019-12-04 21:25:06 -0800

    fixed datetime runtime error

--- vendors/views.py
+++ vendors/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime
+from django.utils import timezone
 
 from vendors.models import VendorItem
 from vendors.models import Vendor
@@ -15,7 +16,7 @@ class VendorItemListView(LoginRequiredMixin, ListView):
     
     def get_queryset(self):                
         uservendors = self.request.user.employer.all()
-        current_date = datetime.now()        
+        current_date = datetime.now(tz=timezone.utc)        
         return VendorItem.objects.select_related('vendor').filter(available_on__lte=current_date, vendor__in=uservendors)
 
 class VendorItemUpdateView(LoginRequiredMixin, UpdateView):
