4335f49da5dbcac3aca9115b4af1e04c54f97f27
bhadley <pebkak@gmail.com>
2019-12-04 21:46:45 -0800

    add pagination
    added paginate to 20 to vendoritem view
    added template partial for pagination controls

--- vendors/views.py
+++ vendors/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 from vendors.models import VendorItem
 from vendors.models import Vendor
@@ -13,14 +14,19 @@ from vendors.models import Vendor
 class VendorItemListView(LoginRequiredMixin, ListView):
     model = VendorItem
     context_object_name = 'vendor_items'
-    
+    paginate_by = 20
+
     def get_queryset(self):                
         uservendors = self.request.user.employer.all()
         current_date = datetime.now(tz=timezone.utc)        
         return VendorItem.objects.select_related('vendor').filter(available_on__lte=current_date, vendor__in=uservendors)
+
+
 
 class VendorItemUpdateView(LoginRequiredMixin, UpdateView):
     model = VendorItem
     context_object_name = 'hotdog'
     fields = ['name']
     success_url = reverse_lazy('vendor-items')
+
+    
