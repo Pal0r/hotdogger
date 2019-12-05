eb5cf25800df3ae27be5fcd61fdeee06550ae57b
bhadley <pebkak@gmail.com>
2019-12-04 20:18:54 -0800

    filtered items view down to employee
    updated to filter down the returned items to the logged in users allowed list of vendors only

--- vendors/views.py
+++ vendors/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from vendors.models import VendorItem
+from vendors.models import Vendor
 
 
 
@@ -10,7 +11,10 @@ from vendors.models import VendorItem
 class VendorItemListView(LoginRequiredMixin, ListView):
     model = VendorItem
     context_object_name = 'vendor_items'
-
+    
+    def get_queryset(self):                
+        uservendors = self.request.user.employer.all()        
+        return VendorItem.objects.filter(vendor__in=uservendors)
 
 class VendorItemUpdateView(LoginRequiredMixin, UpdateView):
     model = VendorItem
